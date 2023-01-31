6a626041e74d91b965f0ca53529d05cbe6b679c8
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-02-01 00:22:09 +0100

    feat(auth): add password visibility toggler

--- src/contexts/userContext.ts
+++ src/contexts/userContext.ts
@@ -25,10 +25,76 @@ export interface NetlifyIdentity {
 // You must run this once before trying to interact with the widget
 netlifyIdentity.init({ locale: 'fr' });
 
+function buildPasswordTogglerFragmentFor(
+  inputElement: HTMLInputElement,
+  iframeDocument: Document | undefined,
+): DocumentFragment | null {
+  const fragmentId = 'password-visibility-toggler';
+
+  if (!iframeDocument || iframeDocument.querySelector(`#${fragmentId}`)) {
+    return null;
+  }
+
+  const fragment = iframeDocument.createDocumentFragment();
+  const div = iframeDocument.createElement('div');
+  div.setAttribute('id', fragmentId);
+  div.style.float = 'right';
+  div.style.minWidth = '50px';
+  div.style.height = '20px';
+  div.style.marginBottom = '12px';
+
+  const link = iframeDocument.createElement('a');
+  link.setAttribute('href', '#');
+  link.style.textDecoration = 'none';
+
+  const span = iframeDocument.createElement('span');
+  span.textContent = 'Afficher le mot de passe';
+  link.appendChild(span);
+  div.append(link);
+
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const inputType = inputElement.getAttribute('type');
+    if (inputType === 'password') {
+      inputElement.setAttribute('type', 'text');
+      span.textContent = 'Masquer le mot de passe';
+      return;
+    }
+
+    inputElement.setAttribute('type', 'password');
+    span.textContent = 'Afficher le mot de passe';
+    return;
+  });
+
+  fragment.appendChild(div);
+  return fragment;
+}
+
+function addPaswwordVisibilityToggler(): void {
+  try {
+    const netlifyFrames = Array.from(document.querySelectorAll('iframe#netlify-identity-widget'));
+    const iframe: HTMLIFrameElement | null = netlifyFrames.pop() as HTMLIFrameElement;
+    const iframeDocument = iframe?.contentDocument || iframe?.contentWindow?.document;
+
+    const passwordInput: HTMLInputElement | null = iframeDocument?.querySelector(
+      'input[type="password"]',
+    ) as HTMLInputElement;
+    const inputContainer = passwordInput?.parentElement as HTMLLabelElement;
+    const fragment = buildPasswordTogglerFragmentFor(passwordInput, iframeDocument);
+
+    if (inputContainer && fragment) {
+      passwordInput.parentElement?.appendChild(fragment);
+    }
+  } catch (error) {
+    // do nothing
+  }
+}
+
 export const userContextInfo: UserInfo = {
   isAuthenticated: currentUser() !== null,
   user: currentUser(),
   login: () => {
+    addPaswwordVisibilityToggler();
     netlifyIdentity.open('login');
     const hamburgerMenuCloseButton: HTMLButtonElement | null = document?.querySelector(
       'button#hamburger-close-button',
