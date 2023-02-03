80b83c3931fa85aa597b7599cb62a425d6998e06
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-02-03 10:26:55 +0100

    feat(qui-suis-je): enhance design

--- src/components/QuiSuisJe.tsx
+++ src/components/QuiSuisJe.tsx
@@ -17,19 +17,21 @@ export const QuiSuisJe: FC = () => {
       >
         <div
           style={{
-            background: 'linear-gradient(to bottom,rgba(0, 0, 0, 0.8), rgba(79, 79, 93, 0.70))',
+            background: 'linear-gradient(to bottom,rgba(0, 0, 0, 0.1), rgba(79, 79, 93, 0.50))',
             height: '100vh',
           }}
         >
           <div className="container h-100 d-flex flex-column justify-content-between align-items-center overflow-y-scroll">
-            <div className="mt-4">
-              <span className="text-uppercase fs-1 fw-bolder font-raleway">Marie-Anne Sévin</span>
+            <div className="mt-2">
+              <span className="text-uppercase text-light fs-1 fw-bolder font-raleway">
+                Marie-Anne Sévin
+              </span>
             </div>
             <div>
-              <span className="fs-2 fw-bolder font-monserrat">Qui suis-je ?</span>
+              <span className="fs-2 fw-bolder text-light font-monserrat">Qui suis-je ?</span>
             </div>
-
-            <div className="fs-5 fw-bolder font-playfair mt-4 mx-4 text-start">
+            <div className="" style={{ minHeight: '11rem' }}></div>
+            <div className="fs-5 fw-bolder font-playfair mt-4 mx-4 text-start text-light">
               <p className="">
                 Musicienne, masseuse, thérapeute, formatrice et auteure, je conçois et j'anime des
                 ateliers pédagogiques et thérapeutiques.
