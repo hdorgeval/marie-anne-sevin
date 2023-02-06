c6edb61dea01ec2a262c0bb08f9cd1ebf80fe53a
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-02-06 23:41:20 +0100

    feat(hooks): create useTitle

---
+++ src/hooks/useTitle.tsx
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+export const useTitle = (htmlPageTitle: string) => {
+  useEffect(() => {
+    const previousTitle = document?.title;
+    if (document) {
+      document.title = htmlPageTitle;
+    }
+
+    return () => {
+      if (document) {
+        document.title = previousTitle;
+      }
+    };
+  });
+};
