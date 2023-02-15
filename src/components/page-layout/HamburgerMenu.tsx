e1c536c7d484ce3c3233ed013ae534ff686510e0
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-02-15 23:35:03 +0100

    feat(burger-menu): add home icon on all pages except home page

--- src/components/page-layout/HamburgerMenu.tsx
+++ src/components/page-layout/HamburgerMenu.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react';
-import { Link } from 'react-router-dom';
+import { FC, useMemo } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import {
   AccountFullNameBadge,
   CreateAccountBadge,
@@ -8,9 +8,21 @@ import {
 } from '../auth/MyAccount';
 
 export const HambugerMenu: FC = () => {
+  const location = useLocation();
+  const isOnHomePage = useMemo(() => {
+    return location.pathname === '/';
+  }, [location.pathname]);
+
   return (
-    <nav className="navbar navbar-dark sticky-top position-absolute top-0 end-0">
-      <div className="container-fluid pe-0 me-1">
+    <nav className="navbar navbar-dark sticky-top position-absolute top-0 end-0 w-100">
+      <div className="container-fluid pe-0 me-1 d-flex flex-row">
+        {isOnHomePage ? (
+          <div className="flex-grow-1"></div>
+        ) : (
+          <Link to="/" className="navbar-brand text-reset">
+            <i className="bi bi-house-up fs-3"></i>
+          </Link>
+        )}
         <button
           className="navbar-toggler"
           type="button"
@@ -20,6 +32,7 @@ export const HambugerMenu: FC = () => {
         >
           <span className="navbar-toggler-icon fs-8"></span>
         </button>
+
         <div
           className="offcanvas offcanvas-end text-bg-dark"
           tabIndex={-1}
@@ -44,7 +57,7 @@ export const HambugerMenu: FC = () => {
                 <div className="d-flex p-1 flex-row flex-wrap align-items-center">
                   <span className="badge rounded-pill text-bg-dark fs-6 border border-secondary m-1">
                     <Link to="/" className="text-decoration-none text-light" aria-current="page">
-                      Acceuil
+                      Accueil
                     </Link>
                   </span>
                   <span className="badge rounded-pill text-bg-dark fs-6 border border-secondary m-1">
@@ -104,11 +117,11 @@ export const HambugerMenu: FC = () => {
                 </div>
               </li>
               <li className="nav-item w-100 text-start mt-4">
-                <div className="w-100 border-bottom border-secondary fs-3 text-secondary d-flex p-1 flex-row flex-wrap align-items-center">
+                <div className="w-100 border-bottom border-secondary fs-6 text-secondary d-flex p-1 flex-row flex-wrap align-items-center">
                   <span>Crédits photos</span>
                 </div>
                 <div className="d-flex p-1 flex-row flex-wrap align-items-center">
-                  <span className="badge rounded-pill text-bg-dark fs-6 border border-secondary m-1">
+                  <span className="badge rounded-pill text-bg-dark fs-7 border border-secondary m-1">
                     Nicolas Rilat photography
                   </span>
                 </div>
