f01b9beaa3af2597e81fb64a4691582ed7aa701a
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-03-29 21:44:59 +0200

    chore(build): create separate vendor chunks

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          ['intro-js']: ['intro.js'],
+          ['use-local-storage-state']: ['use-local-storage-state'],
+          ['netlify-identity-widget']: ['netlify-identity-widget'],
+        },
+      },
+    },
+  },
 });
