6e474b2004e44dd23e9ce50881c56999bc6213a5
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-04-12 23:14:14 +0200

    feat(constellations): update page content

---
+++ src/components/ReadMoreReadLess.tsx
@@ -0,0 +1,83 @@
+import React from 'react';
+
+export interface ReadMoreReadLessProps {
+  height: string;
+  readMoreText: string;
+  readLessText: string;
+  children?: React.ReactNode;
+}
+interface ReadMoreProps {
+  height: string;
+  readMoreText: string;
+  handleToggle: () => void;
+  children?: React.ReactNode;
+}
+
+interface ReadLessProps {
+  height: string;
+  readLessText: string;
+  handleToggle: () => void;
+  children?: React.ReactNode;
+}
+const ReadMore: React.FC<ReadMoreProps> = ({ children, height, handleToggle, readMoreText }) => {
+  return (
+    <div className="overflow-hidden position-relative" style={{ maxHeight: height }}>
+      {children}
+      <div className="position-absolute bottom-0 start-0 w-100 mb-0 h-50"></div>
+      <span
+        aria-label={readMoreText}
+        onClick={handleToggle}
+        role="button"
+        className="badge rounded-pill fs-6 border border-secondary start-50 translate-middle bg-dark position-absolute bottom-0 mb-0 py-2"
+        style={{
+          background: 'linear-gradient(to bottom,rgba(0, 0, 0, 1), rgba(79, 79, 93, 0.3))',
+        }}
+      >
+        <i className="bi bi-arrow-bar-down fs-5 me-2"></i>
+        <span className="me-1">{readMoreText}</span>
+      </span>
+    </div>
+  );
+};
+
+const ReadLess: React.FC<ReadLessProps> = ({ children, readLessText, handleToggle }) => {
+  return (
+    <>
+      {children}
+      <div className="mx-auto w-25 d-flex justify-content-center">
+        <div
+          onClick={handleToggle}
+          role="button"
+          className="badge rounded-pill fs-7 border border-secondary bg-dark mb-0 py-2"
+        >
+          <i className="bi bi-arrow-bar-up fs-7 me-1"></i>
+          {readLessText}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export const ReadMoreReadLess: React.FC<ReadMoreReadLessProps> = ({
+  children,
+  height,
+  readMoreText,
+  readLessText,
+}) => {
+  const [isReadMore, setIsReadMore] = React.useState(true);
+
+  const handleToggle = React.useCallback(() => {
+    setIsReadMore(!isReadMore);
+  }, [isReadMore]);
+  return isReadMore ? (
+    <ReadMore readMoreText={readMoreText} handleToggle={handleToggle} height={height}>
+      {children}
+    </ReadMore>
+  ) : (
+    <ReadLess readLessText={readLessText} handleToggle={handleToggle} height={height}>
+      {children}
+    </ReadLess>
+  );
+};
+
+ReadMoreReadLess.displayName = 'ReadMoreReadLess';
--- src/pages/Constellations.tsx
+++ src/pages/Constellations.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import { Description } from '../components/Description';
 import { OpenExternalLinkButton } from '../components/OpenExternalLinkButton';
+import { ReadMoreReadLess } from '../components/ReadMoreReadLess';
 import { ENTRETIEN_PREALABLE_CALENDAR_LINK } from './page-layout/CalendarLinks';
 import { PageSubTitle } from './page-layout/PageSubTitle';
 import { PageTitle } from './page-layout/PageTitle';
@@ -35,12 +36,10 @@ export const Constellations: FC = () => {
           <div className="d-flex px-0 pt-4 flex-column justify-content-between">
             <SemiTransparentTile className="card text-start text-light font-monserrat">
               <div className="card-body">
-                <h5 className="card-title mb-0 fw-bolder">
-                  Constellation familiale individuelle en visio
-                </h5>
+                <h5 className="card-title mb-0 fw-bolder">Constellation individuelle en visio</h5>
               </div>
               <ul className="list-group list-group-flush mt-0 pt-0">
-                <TransparentListGroupItem className="pt-3">
+                <TransparentListGroupItem className="pt-2 pb-1">
                   <div className="d-flex flex-row align-items-center card-subtitle">
                     <i className="bi bi-clock-history fs-3 me-4"></i>
                     <div className="d-flex flex-column card-subtitle pt-1">
@@ -85,16 +84,33 @@ export const Constellations: FC = () => {
                 </TransparentListGroupItem>
               </ul>
               <div className="card-body pt-2">
-                <Description>...</Description>
+                <ReadMoreReadLess height="100px" readMoreText="Lire plus" readLessText="Lire moins">
+                  <Description>
+                    Les constellations familiales et systémiques sont un outil de clarification et
+                    d'accompagnement de vos problématiques. Que vous ayez des difficultés au niveau
+                    relationnel, au niveau professionnel, des conditionnements ou schémas répétitifs
+                    qui vous encombrent, nous cherchons ensemble à remettre de l'ordre dans votre
+                    système et à faire circuler reconnaissance et amour. Comment cela se passe-t-il
+                    ? Lors d'un entretien de clarification de la problématique, nous identifions
+                    ensemble les personnes ou concepts à représenter dans votre système. Nous
+                    modélisons ce dernier en plaçant les différents éléments dans le champ de
+                    constellations (soit des représentants physiques, soit des objets ou encore des
+                    papiers) et nous ressentons les tensions ou les facilités, ce qui ce joue dans
+                    le système. Nous observons le système évoluer pas à pas, en orientant vers un
+                    apaisement de votre représentant.
+                  </Description>
+                </ReadMoreReadLess>
               </div>
             </SemiTransparentTile>
 
-            <SemiTransparentTile className="card text-start text-light font-monserrat mt-4">
+            <SemiTransparentTile className="card text-start text-light font-monserrat">
               <div className="card-body">
-                <h5 className="card-title mb-0 fw-bolder">Constellation familiale de groupe</h5>
+                <h5 className="card-title mb-0 fw-bolder">
+                  Constellation individuelle en présentiel
+                </h5>
               </div>
               <ul className="list-group list-group-flush mt-0 pt-0">
-                <TransparentListGroupItem className="pt-3">
+                <TransparentListGroupItem className="pt-2 pb-1">
                   <div className="d-flex flex-row align-items-center card-subtitle">
                     <i className="bi bi-clock-history fs-3 me-4"></i>
                     <div className="d-flex flex-column card-subtitle pt-1">
@@ -107,31 +123,153 @@ export const Constellations: FC = () => {
                   <div className="d-flex flex-row align-items-center card-subtitle">
                     <i className="bi bi-geo-alt fs-3 me-4"></i>
                     <div className="d-flex flex-column card-subtitle">
-                      <span className=" fs-7">
-                        En Visio ou en présentiel en fonction de votre demande
+                      <span className="text-nowrap fs-7">
+                        17 rue Philippe de Dangeau, Versailles (78)
                       </span>
+                      <span className="text-nowrap fs-7">5 rue Edmond Rostand, Plaisir (78)</span>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+                <TransparentListGroupItem className="pt-1 pb-0">
+                  <div className="d-flex flex-row card-subtitle align-items-center">
+                    <i className="bi bi-currency-euro fs-3 me-4"></i>
+                    <div className="d-flex flex-column justify-content-between ">
+                      <span className="">120€</span>
                     </div>
                   </div>
                 </TransparentListGroupItem>
                 <TransparentListGroupItem className="py-3">
                   <div className="d-flex flex-row align-items-center card-subtitle">
-                    <i className="bi bi-exclamation-diamond fs-2 me-4"></i>
+                    <i className="bi bi-calendar-event fs-2 me-4"></i>
                     <div className="d-flex flex-column card-subtitle">
-                      <span className="">Rdv validé uniquement après entretien téléphonique</span>
                       <OpenExternalLinkButton
                         className="btn btn-outline-light fw-bolder w-100 mt-1"
                         link={ENTRETIEN_PREALABLE_CALENDAR_LINK}
                         analyticsEvent="rdv-constellation"
-                        title="Prendre rendez-vous pour l'entretien préalable"
+                        title="Prendre rendez-vous"
                       >
-                        Prendre rendez-vous pour l'entretien préalable
+                        Prendre rendez-vous
+                      </OpenExternalLinkButton>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+              </ul>
+            </SemiTransparentTile>
+
+            <SemiTransparentTile className="card text-start text-light font-monserrat mt-4">
+              <div className="card-body">
+                <h5 className="card-title mb-0 fw-bolder">Constellation de groupe en visio</h5>
+              </div>
+              <ul className="list-group list-group-flush mt-0 pt-0">
+                <TransparentListGroupItem className="pt-2 pb-1">
+                  <div className="d-flex flex-row align-items-center card-subtitle">
+                    <i className="bi bi-clock-history fs-3 me-4"></i>
+                    <div className="d-flex flex-column card-subtitle pt-1">
+                      <span className="text-nowrap fs-7">2h</span>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+
+                <TransparentListGroupItem className="mt-0 pt-3">
+                  <div className="d-flex flex-row align-items-center card-subtitle">
+                    <i className="bi bi-geo-alt fs-3 me-4"></i>
+                    <div className="d-flex flex-column card-subtitle">
+                      <span className="text-nowrap fs-7">En Visio</span>
+                      <span className="text-nowrap fs-7">
+                        Le lien sera fourni après inscription
+                      </span>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+                <TransparentListGroupItem className="pt-2 pb-1">
+                  <div className="d-flex flex-row card-subtitle align-items-center">
+                    <i className="bi bi-currency-euro fs-3 me-4"></i>
+                    <div className="d-flex flex-column justify-content-between ">
+                      <span className="">120€ pour vous et gratuit pour les représentants</span>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+                <TransparentListGroupItem className="py-3">
+                  <div className="d-flex flex-row align-items-center card-subtitle">
+                    <i className="bi bi-calendar-event fs-2 me-4"></i>
+                    <div className="d-flex flex-column card-subtitle">
+                      <OpenExternalLinkButton
+                        className="btn btn-outline-light fw-bolder w-100 mt-1"
+                        link={ENTRETIEN_PREALABLE_CALENDAR_LINK}
+                        analyticsEvent="rdv-constellation"
+                        title="Prendre rendez-vous"
+                      >
+                        Prendre rendez-vous
                       </OpenExternalLinkButton>
                     </div>
                   </div>
                 </TransparentListGroupItem>
               </ul>
               <div className="card-body pt-2">
-                <Description>...</Description>
+                <Description>
+                  Nous prenons le temps d'un entretien seul à seul et convenons d'une date pour la
+                  constellation. Nous lançons un appel sur les différents réseaux pour trouver des
+                  représentants qui se connecteront le jour J pour environs 2h de visio
+                </Description>
+              </div>
+            </SemiTransparentTile>
+
+            <SemiTransparentTile className="card text-start text-light font-monserrat mt-4">
+              <div className="card-body">
+                <h5 className="card-title mb-0 fw-bolder">Constellation de groupe en présentiel</h5>
+              </div>
+              <ul className="list-group list-group-flush mt-0 pt-0">
+                <TransparentListGroupItem className="pt-2 pb-1">
+                  <div className="d-flex flex-row align-items-center card-subtitle">
+                    <i className="bi bi-clock-history fs-3 me-4"></i>
+                    <div className="d-flex flex-column card-subtitle pt-1">
+                      <span className="text-nowrap fs-7">2h</span>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+
+                <TransparentListGroupItem className="mt-0 pt-2 pb-1">
+                  <div className="d-flex flex-row align-items-center card-subtitle">
+                    <i className="bi bi-geo-alt fs-3 me-4"></i>
+                    <div className="d-flex flex-column card-subtitle">
+                      <span className="text-nowrap fs-7">
+                        A définir en fonction de la taille du groupe
+                      </span>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+                <TransparentListGroupItem className="pt-2 pb-1">
+                  <div className="d-flex flex-row card-subtitle align-items-center">
+                    <i className="bi bi-currency-euro fs-3 me-4"></i>
+                    <div className="d-flex flex-column justify-content-between ">
+                      <span className="">90€ pour vous et 15€ par représentant (minimum 5)</span>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+                <TransparentListGroupItem className="py-3">
+                  <div className="d-flex flex-row align-items-center card-subtitle">
+                    <i className="bi bi-calendar-event fs-2 me-4"></i>
+                    <div className="d-flex flex-column card-subtitle">
+                      <OpenExternalLinkButton
+                        className="btn btn-outline-light fw-bolder w-100 mt-1"
+                        link={ENTRETIEN_PREALABLE_CALENDAR_LINK}
+                        analyticsEvent="rdv-constellation"
+                        title="Prendre rendez-vous"
+                      >
+                        Prendre rendez-vous
+                      </OpenExternalLinkButton>
+                    </div>
+                  </div>
+                </TransparentListGroupItem>
+              </ul>
+              <div className="card-body pt-2">
+                <Description>
+                  Nous prenons une date pour le mois d'après et je cherche un local dans lequel
+                  pourra avoir lieu la constellation. Nous communiquons notre recherche de
+                  représentants sur les réseaux qui s'engagent à vous accompagner le jour J pour 2h.
+                  Ils profitent également des prises de conscience et ce travail agit sur l'ensemble
+                  des participants de part les effets miroirs.
+                </Description>
               </div>
             </SemiTransparentTile>
           </div>
