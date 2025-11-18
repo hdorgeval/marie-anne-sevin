5afdd915c853eb6914183a22c055eb1c8be1132c
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2025-11-18 11:54:52 +0100

    chore(build): ignore legal comments

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	esbuild: {
+		legalComments: "none",
+	},
 	build: {
 		rollupOptions: {
 			output: {
