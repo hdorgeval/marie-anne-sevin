0e6ff59761b75eab8e7db12973d7738c2fbdc4cf
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2025-11-18 17:20:46 +0100

    chore(npm): update '@netlify/functions' dependency to latest

--- netlify/functions/hello-world.ts
+++ netlify/functions/hello-world.ts
@@ -1,8 +1,8 @@
-import type { Handler } from '@netlify/functions';
+import type { Handler } from "@netlify/functions";
 
 export const handler: Handler = async () => {
-  return {
-    statusCode: 200,
-    body: 'hello TypeScript',
-  };
+	return {
+		statusCode: 200,
+		body: "hello TypeScript",
+	};
 };
