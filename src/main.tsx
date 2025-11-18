b87cbd9da4ed8c5f0abaadc93056e7781ebb4bf9
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2025-11-18 17:49:55 +0100

    chore(build): bundle bootstrap into app

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
-import 'bootstrap-icons/font/bootstrap-icons.css';
-import 'intro.js/introjs.css';
-import 'intro.js/themes/introjs-modern.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './App.css';
-import './bs-extensions.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap-icons/font/bootstrap-icons.min.css";
+import "intro.js/introjs.css";
+import "intro.js/themes/introjs-modern.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./App.css";
+import "./bs-extensions.css";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 );
