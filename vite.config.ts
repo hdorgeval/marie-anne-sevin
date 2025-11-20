42c2cd47177677e760c15436f029081f7b90b2ea
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2025-11-20 10:27:26 +0100

    chore(npm): migrate to react 19

--- vite.config.ts
+++ vite.config.ts
@@ -10,10 +10,41 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			output: {
-				manualChunks: {
-					"intro-js": ["intro.js"],
-					"use-local-storage-state": ["use-local-storage-state"],
-					"vidstack-player": ["@vidstack/react"],
+				manualChunks(id) {
+					if (!id.includes("node_modules")) return;
+
+					if (id.includes("@vidstack")) {
+						return "vidstack-vendor";
+					}
+
+					if (id.includes("intro.js") || id.includes("introjs")) {
+						return "introjs-vendor";
+					}
+
+					if (
+						id.includes("react/") ||
+						id.includes("react-dom") ||
+						id.includes("scheduler")
+					) {
+						return "react-vendor";
+					}
+
+					// --- CHUNK BOOTSTRAP ---
+					// Covers:
+					//  - bootstrap/dist/css/bootstrap*
+					//  - bootstrap/dist/js/bootstrap*
+					//  - bootstrap/util/*
+					if (id.includes("bootstrap")) {
+						return "bootstrap-vendor";
+					}
+
+					// --- CHUNK BOOTSTRAP ICONS ---
+					if (id.includes("bootstrap-icons")) {
+						return "bootstrap-icons-vendor";
+					}
+
+					// --- CHUNK VENDOR PAR DÉFAUT ---
+					return "vendor";
 				},
 			},
 		},
