89a119c736b4293d2f25613d12594d61f11d7ed9
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2025-11-20 16:01:14 +0100

    fix(app): fix markup on error page

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,61 +1,57 @@
-import type { FC } from 'react';
-import { isRouteErrorResponse, Link, useRouteError } from 'react-router-dom';
+import type { FC } from "react";
+import { isRouteErrorResponse, Link, useRouteError } from "react-router-dom";
 
 export interface ErrorResponse {
-  status: number;
-  statusText: string;
-  data: unknown;
-  error?: Error;
-  internal: boolean;
+	status: number;
+	statusText: string;
+	data: unknown;
+	error?: Error;
+	internal: boolean;
 }
 
 const ErrorResponseBoundary: FC<ErrorResponse> = (error: ErrorResponse) => {
-  return (
-    <div
-      className="container w-100 d-flex flex-column justify-content-center"
-      style={{ height: '100vh' }}
-    >
-      <h1>Oops!</h1>
-      <p>Une erreur est survenue !!!</p>
-      {error.status === 404 && <p>La page que vous avez demandée n'existe pas.</p>}
-      <div>
-        <Link to="/">
-          <div className="w-100 d-flex flex-row justify-content-center align-items-center">
-            <span>Retourner à la page d'acceuil</span>
-            <i className="bi bi-box-arrow-in-right fs-1 ms-2"></i>
-          </div>
-        </Link>
-      </div>
-    </div>
-  );
+	return (
+		<div
+			className="container w-100 d-flex flex-column justify-content-center"
+			style={{ height: "100vh" }}
+		>
+			<h1>Oops!</h1>
+			<p>Une erreur est survenue !!!</p>
+			{error.status === 404 && (
+				<p>La page que vous avez demandée n'existe pas.</p>
+			)}
+			<div>
+				<Link to="/" className="btn btn-primary mt-3">
+					Retour à l’accueil
+				</Link>
+			</div>
+		</div>
+	);
 };
 const GenericErrorBoundary: FC = () => {
-  return (
-    <div
-      className="container w-100 d-flex flex-column justify-items-center"
-      style={{ height: '100vh' }}
-    >
-      <h1>Oops!</h1>
-      <p>Une erreur est survenue !!!</p>
-      <p>
-        <Link to="/">
-          <div className="w-100 d-flex flex-row justify-content-center align-items-center">
-            <span>Retourner à la page d'acceuil</span>
-            <i className="bi bi-box-arrow-in-right fs-1 ms-2"></i>
-          </div>
-        </Link>
-      </p>
-    </div>
-  );
+	return (
+		<div
+			className="container w-100 d-flex flex-column justify-items-center"
+			style={{ height: "100vh" }}
+		>
+			<h1>Oops!</h1>
+			<p>Une erreur est survenue !!!</p>
+			<div>
+				<Link to="/" className="btn btn-primary mt-3">
+					Retour à l’accueil
+				</Link>
+			</div>
+		</div>
+	);
 };
 
 export const ErrorPage: FC = () => {
-  const error = useRouteError() as ErrorResponse;
-  if (isRouteErrorResponse(error)) {
-    return <ErrorResponseBoundary {...error} />;
-  }
+	const error = useRouteError() as ErrorResponse;
+	if (isRouteErrorResponse(error)) {
+		return <ErrorResponseBoundary {...error} />;
+	}
 
-  return <GenericErrorBoundary />;
+	return <GenericErrorBoundary />;
 };
 
-ErrorPage.displayName = 'ErrorPage';
+ErrorPage.displayName = "ErrorPage";
